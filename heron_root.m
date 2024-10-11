cd41c0c6522de18ff8fc43b4312d333e753c8e1d
Lloyd Sangwoo Ko <sangwoo_ko@brown.edu>
2024-10-10 22:52:15 -0400

    Update heron_root.m

--- heron_root.m
+++ heron_root.m
@@ -1,14 +1,18 @@
 function [S_root] = heron_root(S, tol)
 
 x_old = S;
-x_new = 0.5 * (x_old + S / x_old);
 
-while abs(x_new - x_old) > tol
-    x_old = x_new;
-    x_new = 0.5 * (x_old + S / x_old);
+for n = 1:5000 
+x_new = (1/2)*(x_old + (S/x_old));
+
+    if abs(x_new - x_old) < tol
+        break;
+    end
+x_old = x_new;
+
 end
 
-S_root = x_new;
+S_root = x_new; 
 
 disp(S_root);
 
